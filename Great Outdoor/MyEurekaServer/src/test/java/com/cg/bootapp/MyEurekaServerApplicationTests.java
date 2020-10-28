81257fd87bdc73b0e3b8b48a077b80c6872fdc23
Shivani Mane <48205829+shivanimane@users.noreply.github.com>
2020-10-28 09:04:52 +0530

    Changes to check

---
+++ Great Outdoor/MyEurekaServer/src/test/java/com/cg/bootapp/MyEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.bootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
