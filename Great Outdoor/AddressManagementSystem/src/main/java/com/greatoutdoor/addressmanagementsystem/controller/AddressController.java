a89c8c57d693284d73157c2bf6aeca521f8a544e
matkardeepali98 <59158822+matkardeepali98@users.noreply.github.com>
2020-10-28 10:55:11 +0530

    AddressManagementSystem

---
+++ Great Outdoor/AddressManagementSystem/src/main/java/com/greatoutdoor/addressmanagementsystem/controller/AddressController.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package com.greatoutdoor.addressmanagementsystem.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.greatoutdoor.addressmanagementsystem.model.Address;
+import com.greatoutdoor.addressmanagementsystem.service.AddressService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/address")
+public class AddressController {	
+	
+	@Autowired
+	AddressService addressService;
+	
+	@GetMapping("/viewALLAddress")
+	List<Address>viewAllAddress(){
+		return addressService.viewAllAddresss();
+	}
+
+	@PostMapping("/addAddress")
+	String addAddress(@RequestBody Address address) {
+		String status="Address added";
+		
+		if(addressService.addAddress(address)) {
+		return status;
+		}	
+		return "fail to add Address";
+	}
+	
+	@PostMapping("/updateAddress")
+	String updateAddress(@RequestBody Address address) {
+		String status="Address Updated";
+		
+		if(addressService.updateAddress(address)) {
+			return status;
+		}
+		return "Failed to update Address";
+	}
+	
+	
+	@PostMapping("/deleteAddress")
+	String deleteAddress(@RequestParam String addressId) {
+		if(addressService.deleteAddress(addressId)) {
+			return"Address Deleted Successfully";
+		}
+		return "error";
+	}
+}
---
+++ Great Outdoor/AddressManagementSystem/src/main/java/com/greatoutdoor/addressmanagementsystem/model/Address.java
@@ -0,0 +1,97 @@
+package com.greatoutdoor.addressmanagementsystem.model;
+/**
+ *  @author Deepali
+ */
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="address_ms")
+public class Address {
+	
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private String addressId;
+	private String retailerId;
+	private String buildingNo;
+	private String city;
+	private String state;
+	private String field;
+	private String zip;
+	
+	
+	public Address(String addressId, String retailerId, String buildingNo, String city, String state, String field,
+			String zip) {
+		super();
+		this.addressId = addressId;
+		this.retailerId = retailerId;
+		this.buildingNo = buildingNo;
+		this.city = city;
+		this.state = state;
+		this.field = field;
+		this.zip = zip;
+	}
+
+	public String getAddressId() {
+		return addressId;
+	}
+
+	public void setAddressId(String addressId) {
+		this.addressId = addressId;
+	}
+
+	public String getRetailerId() {
+		return retailerId;
+	}
+
+	public void setRetailerId(String retailerId) {
+		this.retailerId = retailerId;
+	}
+
+	public String getBuildingNo() {
+		return buildingNo;
+	}
+
+	public void setBuildingNo(String buildingNo) {
+		this.buildingNo = buildingNo;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getField() {
+		return field;
+	}
+
+	public void setField(String field) {
+		this.field = field;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public Address() {}
+		
+}
