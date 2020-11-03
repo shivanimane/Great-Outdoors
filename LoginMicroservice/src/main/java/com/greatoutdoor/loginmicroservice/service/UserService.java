59af1d08e5fc90a90f5a734d5b9fb8ef84f3747d
Aashi Mandawat <aashi.mandawat67@gmail.com>
2020-11-03 23:51:18 +0530

    commit

--- LoginMicroservice/src/main/java/com/greatoutdoor/loginmicroservice/service/UserService.java
+++ LoginMicroservice/src/main/java/com/greatoutdoor/loginmicroservice/service/UserService.java
@@ -1,22 +1,18 @@
-<<<<<<< HEAD:LoginMicroservice/src/main/java/com/greatoutdoor/loginservice/service/UserService.java
-package com.greatoutdoor.loginservice.service;
-=======
 package com.greatoutdoor.loginmicroservice.service;
->>>>>>> a21c65d6b019dcae2c87f3ad3d8cabbcb79ce8d1:LoginMicroservice/src/main/java/com/greatoutdoor/loginmicroservice/service/UserService.java
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-<<<<<<< HEAD:LoginMicroservice/src/main/java/com/greatoutdoor/loginservice/service/UserService.java
-import com.greatoutdoor.loginservice.dao.UserRepository;
-import com.greatoutdoor.loginservice.model.User;
-=======
+
+import com.greatoutdoor.loginmicroservice.dao.UserDao;
+import com.greatoutdoor.loginmicroservice.model.User;
+
 import com.greatoutdoor.loginmicroservice.model.SignUpBean;
 import com.greatoutdoor.loginmicroservice.model.User;
 
 import com.greatoutdoor.loginmicroservice.dao.UserDao;
 
->>>>>>> a21c65d6b019dcae2c87f3ad3d8cabbcb79ce8d1:LoginMicroservice/src/main/java/com/greatoutdoor/loginmicroservice/service/UserService.java
+
 
 @Service
 public class UserService {
