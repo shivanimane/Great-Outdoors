dbd3c54c590b884130ecbbc5825cad03a8be2455
matkardeepali98 <59158822+matkardeepali98@users.noreply.github.com>
2020-11-05 12:03:49 +0530

    commit

--- Great Outdoor- Frontend/great-outdoors/src/app/list-wishlist/list-wishlist.component.ts
+++ Great Outdoor- Frontend/great-outdoors/src/app/list-wishlist/list-wishlist.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { WishModel } from '../models/wish.model';
+import { WishService } from '../service/wish.service';
 @Component({
   selector: 'app-list-wishlist',
   templateUrl: './list-wishlist.component.html',
   styleUrls: ['./list-wishlist.component.css']
 })
 export class ListWishlistComponent implements OnInit {
-
-  constructor() { }
+    wishes : WishModel[]=[];
+    constructor(private route : Router,
+      private service:WishService
+      ) { }
 
   ngOnInit(): void {
+    
+  }
+  
+  clickOnViewAllItems(){
+    this.route.navigate(['']);
   }
 
+  clickOnAddToWishlist(){
+    this.route.navigate(['']);
+  }
+
+  clickOnDeleteProduct(){
+    this.route.navigate(['']);
+
+
+  }
+
+  clickOnViewAllProductFromWishList(){
+    this.route.navigate(['']);
+
+  }
+
+
+ 
 }
+
+
+
---
+++ Great Outdoor- Frontend/great-outdoors/src/app/models/wish.model.ts
@@ -0,0 +1,4 @@
+export class WishModel{
+     userId : String;
+     productId : String;
+}
